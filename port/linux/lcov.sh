b8d22ca4010aa6491e4120ca5cc348be2b349e4f
Renzhihan <757419920@qq.com>
2023-04-06 16:12:12 +0800

    lcov

--- port/linux/lcov.sh
+++ port/linux/lcov.sh
@@ -1,3 +1,4 @@
-lcov --capture --directory build/package/pikascript/CMakeFiles/pikascript-core.dir/pikascript-core --output-file  testHtml.info --test-name testHtml
+lcov --capture --directory build/package/pikascript/CMakeFiles/pikascript-core.dir/pikascript-core --directory build/package/pikascript/CMakeFiles/pikascript-core.dir/pikascript-lib/*/ --output-file  testHtml.info --test-name testHtml
+
 genhtml -o result testHtml.info
 
